f8d3d9236ca1616d8d7cdb33b2da656582ae539a
JM <jmnavarro@gmail.com>
2024-03-21 09:25:05 -0400

    works?

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    headers: [
-        ...
-        {
-          key: 'Access-Control-Allow-Origin',
-          value: '*',
-        },
-    ],
-};
-
-export default nextConfig;
+module.exports = {
+    async headers() {
+        return [
+          {
+            source: "*",
+            headers: [
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "*", // Set your origin
+              },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET, POST, PUT, DELETE, OPTIONS",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value: "Content-Type, Authorization",
+              },
+            ],
+          },
+        ];
+      },    
+}
