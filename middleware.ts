a427f74cc51717e77de84d4110e504d1804cb25e
JM <jmnavarro@gmail.com>
2024-03-21 10:48:04 -0400

    remove hokkdeck

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { withHookdeck } from 'vercel-integration-demo'
 
 export const config = {
   matcher: '/webhook-handler',
 };
  
-function middleware(request: NextRequest) {
+ export default function middleware(request: NextRequest) {
   console.log(`Middleware call from ${request.url.toString()}`); 
 
   const requestHeaders = new Headers(request.headers);
@@ -18,5 +17,3 @@ function middleware(request: NextRequest) {
     },
   });  
 }
-
-export default withHookdeck(middleware);
