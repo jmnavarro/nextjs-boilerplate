cbe40082e02ea6df2b62c9658575e3b03786156a
JM <jmnavarro@gmail.com>
2024-03-21 08:04:17 -0400

    show user agent

--- app/webhook-handler/page.tsx
+++ app/webhook-handler/page.tsx
@@ -2,7 +2,7 @@ import { headers } from 'next/headers'
 
 export default function WebhookHandlerPage() {
     const headersList = headers();
-    const referer = headersList.get('referer');
+    const referer = headersList.get('user-agent');
 
     return (
         <div>from {referer}</div>
