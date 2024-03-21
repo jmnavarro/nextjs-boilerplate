c63778cee0735e95d03144229ba9c2f1f0a39374
JM <jmnavarro@gmail.com>
2024-03-21 08:02:30 -0400

    change page

--- app/webhook-handler/page.tsx
+++ app/webhook-handler/page.tsx
@@ -1,5 +1,10 @@
+import { headers } from 'next/headers'
+
 export default function WebhookHandlerPage() {
+    const headersList = headers();
+    const referer = headersList.get('referer');
+
     return (
-      <div><h1>Webhook handler payload</h1></div>
+        <div>from {referer}</div>
     );
   }
