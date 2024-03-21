32fcf8fbffa978f605808f80b25b34bd4068c1d5
JM <jmnavarro@gmail.com>
2024-03-21 09:33:09 -0400

    all routers

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
           {
-            source: "/webhook-handler",
+            source: "/(.*)",
             headers: [
               {
                 key: "Access-Control-Allow-Origin",
