3216c30af53797f5c00bd153a5261e349051876d
JM <jmnavarro@gmail.com>
2024-04-08 06:25:41 -0400

    try 2

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export const config = {
 };
  
 export default function middleware(request: Request, ctx: NextFetchEvent) {
-  console.log(`Middleware call from ${request.url.toString()}: host=${process.env.VERCEL_URL}`); 
+  console.log(`Middleware call from ${request.url.toString()}: VERCEL_URL=${process.env.VERCEL_URL} VERCEL_BRANCH_URL=${process.env.VERCEL_BRANCH_URL}`); 
 
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('user-agent', 'New User Agent overriden by middleware!')
