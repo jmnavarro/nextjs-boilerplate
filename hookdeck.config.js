f88b8f29a1dd7ffa2e42d500cc8ac36608e77b30
JM <jmnavarro@gmail.com>
2024-04-08 08:42:09 -0400

    add hookdeck

---
+++ hookdeck.config.js
@@ -0,0 +1,37 @@
+// TODO: See documentation here https://hookdeck.com/xxxxx
+
+module.exports = {
+  'from-vercel-v035': {
+    match: '/webhook-handler',
+/*
+    // all fields below this line are optional:
+    host: '',
+    retry: {
+      strategy: 'exponential',
+      count: 0,
+      interval: 0,
+    },
+    delay: 0,
+    alert: '',
+    filters: [
+      {
+        headers: {},
+        body: {},
+        query: {},
+        path: {},
+      },
+    ],
+    // source configuration
+    custom_response: {},
+    verification: {},
+    // destination configuration
+    url: '',
+    delivery_rate: {
+      limit: 100,
+      period: 'minute',
+    },
+    http_method: '',
+    auth_method: {},
+*/
+  },
+};
