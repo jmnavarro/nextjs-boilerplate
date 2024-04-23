c0039dfb83afc9bdf40f4a684f01b2b56944532e
JM <jmnavarro@gmail.com>
2024-04-23 14:27:17 -0400

    test config

--- hookdeck.config.js
+++ hookdeck.config.js
@@ -1,6 +1,8 @@
 // TODO: See documentation here https://hookdeck.com/xxxxx
 
 module.exports = {
+  vercel_url: 'https://nextjs-boilerplate-eta-five-30.vercel.app',
+  api_key: process.env.HOOKDECK_API_KEY,
   match: {
     '/webhook-handler': {
       delay: 1000,
