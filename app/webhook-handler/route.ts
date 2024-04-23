8a974c4ff052b198c6962ff11b53bd572232769a
JM <jmnavarro@gmail.com>
2024-04-23 14:11:20 -0400

    enable HD

--- app/webhook-handler/route.ts
+++ app/webhook-handler/route.ts
@@ -3,6 +3,7 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function POST(request: Request) {
+    console.log("POST edge function", request.headers);
     return new Response(JSON.stringify({ from: `POST function from ${process.env.VERCEL_REGION}`}), {
         status: 200,
         headers: { 'content-type': 'application/json' },
@@ -10,6 +11,7 @@ export function POST(request: Request) {
 }
 
 export function PUT(request: Request) {
+    console.log("PUT edge function", request.headers);
     return new Response(JSON.stringify({ from: `PUT function from ${process.env.VERCEL_REGION}`}), {
         status: 200,
         headers: { 'content-type': 'application/json' },
