23f11867cc0e7ef3a7863032168874e1e7669b89
JM <jmnavarro@gmail.com>
2024-04-25 12:49:06 -0400

    add full config

--- hookdeck.config.js
+++ hookdeck.config.js
@@ -2,7 +2,39 @@
 const hookdeckConfig = {
   match : {
     '/webhook-handler': {
-      delay: 5000, // in ms
+      retry: {
+        strategy: RetryStrategy.Linear,
+        count: 5,
+        interval: 1 * 60 * 1000, // in milliseconds
+      },
+      delay: 1 * 60 * 1000, // in milliseconds
+      filters: [
+        {
+          headers: {
+            'x-my-header': 'my-value',
+          },
+          body: {},
+          query: {},
+          path: {},
+        },
+      ],
+      rate: {
+        limit: 10,
+        period: DestinationRateLimitPeriod.Seconds,
+      },
+
+      verification: {
+        type: 'API_KEY',
+        configs: {
+          header_key: 'x-my-api-key',
+          api_key: 'this-is-my-token',
+        }
+      },
+
+      custom_response: {
+        contentType: SourceCustomResponseContentType.Json,
+        body: '{"message": "Vercel handled the webhook using Hookdeck"}',
+      },
     },
   }
 };
